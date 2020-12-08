ae16565abe53bfe629ee546c5e4d9fae3b846fab
cundy01067 <57181197+cundy01067@users.noreply.github.com>
2020-12-08 21:32:18 +0800

    寫控制台

---
+++ HC_TC_2DGame/Assets/Script/TetrisManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TetrisManager : MonoBehaviour
+{
+
+    [Header("掉落時間"), Range(0.1F, 3)]
+    public float timaFall = 1.5F;
+    [Header("目前分數")]
+    public int score;
+    [Header("最高分數")]
+    public int scoreHight;
+    [Header("等級")]
+    public int Lv;
+    [Header("結束畫面")]
+    public GameObject objFinal;
+    [Header("音效:掉落、移動、消除與結束")]
+    public AudioClip SoundFall;
+    public AudioClip SoundMove;
+    public AudioClip SoundClear;
+    public AudioClip SoundLose;
+
+}
