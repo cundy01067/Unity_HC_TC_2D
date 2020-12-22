e247ade26e9eff99157fcf456e5d9ef31da143c5
cundy01067 <57181197+cundy01067@users.noreply.github.com>
2020-12-22 21:37:14 +0800

    12_22

--- HC_TC_2DGame/Assets/Script/TetrisManager.cs
+++ HC_TC_2DGame/Assets/Script/TetrisManager.cs
@@ -22,17 +22,17 @@ public class TetrisManager : MonoBehaviour
     public AudioClip SoundMove;
     public AudioClip SoundClear;
     public AudioClip SoundLose;
-    
+
     /// <summary>
     /// 遊戲開始按鈕_欄位(按鈕_UI)
     /// </summary>
-   [Header("遊戲開始按鈕") ]
+    [Header("遊戲開始按鈕")]
     public Button StartGame;
 
     /// <summary>
     /// 下一個方塊_欄位
     /// </summary>
-    [Header("下一個方塊") , Tooltip("方塊編號")]
+    [Header("下一個方塊"), Tooltip("方塊編號")]
     public Transform TraNextArea;
 
     /// <summary>
@@ -46,23 +46,38 @@ public class TetrisManager : MonoBehaviour
     [Header("畫布")]
     public Transform TraCanvas;
 
+
+    
     #endregion
 
     #region 事件
 
     private void Start()
     {
-        //呼叫方塊生成方法
-        Block_generation();
 
+
+        //按下開始之後呼叫方塊生成方法(Block_generation)
+        StartGame.onClick.AddListener(Block_generation);
+        //按下開始遊戲呼叫(Start_game)方法
         StartGame.onClick.AddListener(Start_game);
+
+        
+
+
 
     }
 
-    
+
     #endregion
 
     #region 方法
+    //判斷開始遊戲是否被按下
+    public bool If_Start_OnClick()
+    {
+        //StartGame.onClick();
+        return true;
+    }
+
     /// <summary>
     /// 遊戲開始按鈕_方法
     /// </summary>
@@ -74,22 +89,27 @@ public class TetrisManager : MonoBehaviour
         GameObject tetris = TraNextArea.GetChild(Index_Next).gameObject;
         //Instantiate(實例化)生成物件 , 放到父物件裡面
         GameObject Current = Instantiate(tetris, TraCanvas);
-        //校準初始生成位置
-        Current.GetComponent<RectTransform>().anchoredPosition = new Vector2(85, 350);
+        //校準初始生成位置  
+        Current.GetComponent<RectTransform>().anchoredPosition = new Vector2(50, 450);
+    }
+
+    public void Update()
+    {
+
     }
     /// <summary>
-    /// 方塊生成
+    /// 生成下一顆方塊
     /// </summary>
     public void Block_generation()
     {
+        
         //給下一顆方塊編號
         Index_Next = Random.Range(0, 12);
         //顯示右邊下一顆方塊
         TraNextArea.GetChild(Index_Next).gameObject.SetActive(true);
+        //必定要傳回
         
 
-
-        
     }
 
     /// <summary>
